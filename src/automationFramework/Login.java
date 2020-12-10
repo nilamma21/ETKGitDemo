968117bf47e2a3fa163a65c686123ed71ce4dff6
nilamma <nilamma@cybage.com>
2020-12-10 12:09:24 +0530

    Added login success msg

--- src/automationFramework/Login.java
+++ src/automationFramework/Login.java
@@ -19,6 +19,8 @@ driver.findElement(By.linkText("Administrator Sign-in")).click();
 driver.findElement(By.xpath("//input[@id='txtUsername']")).sendKeys("shradhama@cybage.com");
 driver.findElement(By.xpath("//input[@id='txtPassword']")).sendKeys("Ma45AzSd27");
 driver.findElement(By.xpath("//input[@id='cmdSubmit']")).click();
+
+System.out.println("Login successfull");
 driver.findElement(By.linkText("RECEIVERTEST")).click();
 driver.quit();
 }
